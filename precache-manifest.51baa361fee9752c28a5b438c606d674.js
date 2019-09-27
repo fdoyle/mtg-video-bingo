4fee9532530110b673036c758b0ee0301f15cd62
Frank Doyle <fdoyl001@gmail.com>
2019-09-26 21:52:56 -0500

    Updates

---
+++ precache-manifest.51baa361fee9752c28a5b438c606d674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11dcd7e8feeb9602e200d33fd9459dcf",
+    "url": "/fdoyle/mtg-video-bingo.git/index.html"
+  },
+  {
+    "revision": "111b3af2b70e23673c56",
+    "url": "/fdoyle/mtg-video-bingo.git/static/css/main.5bfffdc7.chunk.css"
+  },
+  {
+    "revision": "d14122081e8320c84ce1",
+    "url": "/fdoyle/mtg-video-bingo.git/static/js/2.870c94df.chunk.js"
+  },
+  {
+    "revision": "111b3af2b70e23673c56",
+    "url": "/fdoyle/mtg-video-bingo.git/static/js/main.89eacd42.chunk.js"
+  },
+  {
+    "revision": "b2de4444196c980843be",
+    "url": "/fdoyle/mtg-video-bingo.git/static/js/runtime-main.2004077b.js"
+  },
+  {
+    "revision": "52ba0ee408c68cf43114585f72f46c4e",
+    "url": "/fdoyle/mtg-video-bingo.git/static/media/lighthouse.52ba0ee4.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fdoyle/mtg-video-bingo.git/precache-manifest.51baa361fee9752c28a5b438c606d674.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fdoyle/mtg-video-bingo.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
